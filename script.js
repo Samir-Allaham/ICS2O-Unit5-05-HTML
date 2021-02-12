eec8345e9a5ad0791c3491a5478ed78b48a99f4b
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-12 18:21:43 +0000

    fixed indents and spaces

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 let number = 0
-let sentance = 'hello, this is the number 4.'
 
-document.getElementById('report').addEventListener('click', specialButton)
+document.getElementById('report').addEventListener('click', calculator)
 
+document.getElementById('formula').addEventListener('click', formula)
 
-document.getElementById('formula').addEventListener('click', formulaButton)
-
- function formulaButton () {
- number = document.getElementById('input').value
+ function formula () {
+  number = document.getElementById('input').value
  
-
- number = 'The formula for a square is base(height)'
- alert(number)
+  number = 'The formula for a square is base(height)'
+  alert(number)
  }
 
-function specialButton () {
- number = document.getElementById('input').value
+function calculator () {
+  number = document.getElementById('input').value
 
-number = parseInt(number)
+  number = parseInt(number)
 
-number = number * number
+  number = number * number
 
-
-alert(number)
+  alert(number)
 }
