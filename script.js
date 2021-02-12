498c3854f38cd90694118187c5a2fe01c2ff6d0b
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-12 18:26:17 +0000

    spaces

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.getElementById('report').addEventListener('click', calculator)
 
 document.getElementById('formula').addEventListener('click', formula)
 
- function formula () {
+function formula () {
   number = document.getElementById('input').value
- 
+  
   number = 'The formula for a square is base(height)'
   alert(number)
- }
+}
 
 function calculator () {
   number = document.getElementById('input').value
